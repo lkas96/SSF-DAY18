db96636e4db0b0631b90facad72e6b84c2b145f2
lkas96 <kwek_lawson@hotmail.com>
2024-12-05 10:56:17 +0800

    Spring Initializer + Maven Dependencies

---
+++ src/main/java/sg/edu/nus/iss/vttp_day18/VttpDay18Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay18Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp_day18/VttpDay18ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
